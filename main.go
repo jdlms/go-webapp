a3a79947a266de2dd5d192bc7d7fe317125d06cc
jdlms <lamorey.joshua@gmail.com>
2025-05-26 21:15:45 +0200

    StaticHandler + Must func

--- main.go
+++ main.go
@@ -2,44 +2,26 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/jdlms/go-webapp/controllers"
 	"github.com/jdlms/go-webapp/views"
 )
-
-func executeTemplate(w http.ResponseWriter, filePath string) {
-	t, err := views.Parse(filePath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-	t.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/home.gohtml")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/contact.gohtml")
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/faqs.gohtml")
-}
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faqs", faqHandler)
-	r.Get("/faqs/{slug}", faqHandler)
+	tpl := views.Must(views.Parse("templates/home.gohtml"))
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse("templates/contact.gohtml"))
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse("templates/faqs.gohtml"))
+	r.Get("/faqs", controllers.StaticHandler(tpl))
 
 	// page not found
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
--- views/template.go
+++ views/template.go
@@ -7,7 +7,14 @@ import (
 	"net/http"
 )
 
-func Parse(filepath string) (Template, error){
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func Parse(filepath string) (Template, error) {
 	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
@@ -22,7 +29,7 @@ type Template struct {
 }
 
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
- 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	err := t.htmlTpl.Execute(w, nil)
 	if err != nil {
